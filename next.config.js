3c556faaf90d484a2dc88c9ad238db8d13e6a1ee
James Teerayuth <teerayuth.james@gmail.com>
2020-12-16 11:52:30 +0700

    vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
+/*const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
@@ -6,3 +6,4 @@ module.exports = {
   trailingSlash: true,
 }
 
+*/
