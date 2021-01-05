350e0d278555abc8de4e7eac845e2d752df2f70e
mctunt <teerayuth.james@gmail.com>
2021-01-05 15:23:12 +0700

    delete xycrts

--- pages/gold-bid.tsx
+++ pages/gold-bid.tsx
@@ -1,17 +1,14 @@
 import dynamic from 'next/dynamic'
-import Examples from '@components/Charts/Examples'
+//import Examples from '@components/Charts/Examples'
 import ParentSize from '@visx/responsive/lib/components/ParentSize'
 const LayoutApp = dynamic(() => import('@components/Layout'))
+const Examples = dynamic(() => import('@components/Charts/Examples'))
 
 function GoldBid() {
   return (
     <LayoutApp>
       <div className="mt-14">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6">
-          <ParentSize>
-            {({ width }) => <Examples width={width} height={550} />}
-          </ParentSize>
-        </div>
+        <div className="max-w-6xl mx-auto px-4 sm:px-6"></div>
       </div>
     </LayoutApp>
   )
